a5b52317dd8632874ebb85aff084646725f734d8
melisaclskn <melisaclskn5461@gmail.com>
2022-07-12 10:22:03 +0300

    back-top visibility

--- template.js
+++ template.js
@@ -11,3 +11,17 @@ window.onscroll = function() {
   }
 }};
 
+//when scroll , show back top button.
+if (document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280) {
+    
+  var back_top = document.querySelector("#back_top");
+  
+  window.onscroll = function() {
+    "use strict";
+    if (document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280) {
+      back_top.style.display="block";
+    } else {
+      back_top.style.display="none";
+    }
+  }};
+  
