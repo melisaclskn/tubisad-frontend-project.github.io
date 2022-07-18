fd402ef8ea7738774754f4087580e89758f7ab62
melisaclskn <melisaclskn5461@gmail.com>
2022-07-19 00:23:29 +0300

    delete bootstrap script

--- js/template.js
+++ js/template.js
@@ -24,4 +24,39 @@ if (document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280)
       back_top.style.display="none";
     }
   }};
+
+  /****** */
+  $(function () {
+    $("#btnSubmit").click(function () {
+      let email, password;
+      email = jQuery.trim($("#user_email").val());
+      password = jQuery.trim($("#user_password").val());
   
+  
+      //validation  email
+      if (email == "") {
+        $("#validation_email").html("Email boş geçilemez...")
+      } else if (regexEmail(email) == false) {
+        $("#validation_email").html("Uygun formatta email girilmedi exam: deneme@gmail.com")
+      }
+  
+      //regex email
+      function regexEmail(email) {
+        let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(email);
+      }
+  
+      //validation  password
+      if (password == "") {
+        $("#validation_password").html("password boş geçilemez...")
+      } else if (regexPassword(password) == false) {
+        $("#validation_password").html("Uygun formatta  şifre girilmedi exam: en az 8 karakter")
+      }
+  
+      //regex email
+      function regexPassword(password) {
+        let regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;;
+        return regex.test(password);
+      }
+    })//onclick close
+  }); //document ready close
